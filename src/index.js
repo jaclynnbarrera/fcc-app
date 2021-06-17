00005dab1023a14194231bde3be1eadca9c85918
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-06-16 21:54:37 -0400

    add array of book objects, iterate through Array of Objects in Booklist component passing in each book obj as prop to Book component. use destructuring

--- src/index.js
+++ src/index.js
@@ -2,48 +2,40 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-const firstBook = {
-  title: "Caste",
-  author: "Isabel Wilkerson",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51f42uWcUWL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
-};
-
-const secondBook = {
-  title: "Atomic Habits",
-  author: "James Clear",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51vSbWpF+dS._SY344_BO1,204,203,200_.jpg",
-};
+const books = [
+  {
+    title: "Caste",
+    author: "Isabel Wilkerson",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51f42uWcUWL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+  {
+    title: "Atomic Habits",
+    author: "James Clear",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51vSbWpF+dS._SY344_BO1,204,203,200_.jpg",
+  },
+  {
+    title: "Goodnight Moon",
+    author: "Margaret Wise Brown",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51Ix49rxgtL._SY414_BO1,204,203,200_.jpg",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        title={firstBook.title}
-        author={firstBook.author}
-        img={firstBook.img}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic illo
-          nobis nemo provident sequi quia reprehenderit eveniet voluptatibus
-          esse quos.
-        </p>
-      </Book>
-      <Book
-        title={secondBook.title}
-        author={secondBook.author}
-        img={secondBook.img}
-      />
+      {books.map((book) => (
+        <Book book={book} />
+      ))}
     </section>
   );
 }
 
-const Book = ({ img, title, author, children }) => {
+const Book = ({ book: { img, title, author } }) => {
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h3>{author}</h3>
-      {children}
     </article>
   );
 };
